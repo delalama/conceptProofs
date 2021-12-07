ed6e5bb5926ec3d0f8ef419f07cec354736a649c
elaltas <elaltas@gmail.com>
2021-12-07 01:37:38 +0100

    set maven run to change profile fast

---
+++ APIStream/src/test/java/com/dll/APIStream/PersonTest.java
@@ -0,0 +1,15 @@
+package com.dll.APIStream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+
+    @Test
+    public void testPerson() {
+
+        Assertions.assertTrue(1 == 1);
+    }
+
+
+}
